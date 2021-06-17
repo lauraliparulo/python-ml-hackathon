f4fa28144585729f1748d03c3876eb08d63a7439
Liparulo <laura.liparulo@capgemini.com>
2021-06-17 20:55:54 +0200

    gitignore and dockerignore added

--- multiclass_LinearSVC.py
+++ multiclass_LinearSVC.py
@@ -1,4 +1,4 @@
-from data_utils import data_set_test_preparation
+from utils.data_utils import data_set_test_preparation
 from training_models_utils import score_with_LinearSVC
 
 subjectsTest, categoriesTest = data_set_test_preparation("people_csv2")
--- multiclass_random_forest.py
+++ multiclass_random_forest.py
@@ -1,5 +1,5 @@
-from data_utils import data_set_test_preparation
-from training_models_utils import score_with_RandomForest
+from utils.data_utils import data_set_test_preparation
+from utils.training_models_utils import score_with_RandomForest
 
 subjectsTest, categoriesTest = data_set_test_preparation("people_csv2")
 score_with_RandomForest(subjectsTest, categoriesTest)
